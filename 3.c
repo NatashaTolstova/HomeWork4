555ff1eafa1e22d9bbe27ebe1defda453035a393
NATASHA <klimknatasha@yandex.ru>
2024-11-06 23:20:19 +0300

    3

--- 3.c
+++ 3.c
@@ -4,3 +4,30 @@
 Данные на входе: Одно целое число
 Данные на выходе: YES или NO
 */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    int number;
+    bool flag = false;
+
+    scanf("%d", &number);
+
+    while (number > 0)
+    {
+        flag = (number % 2 == 0);
+        
+        if (!flag)
+        {
+            break;
+        }
+        
+        number /= 10;
+    }
+    
+    flag ? printf("YES") : printf("NO");
+
+    return 0;
+}
