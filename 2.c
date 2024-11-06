499e04f3972903a2daddf4d96ed4f1c4df2ead03
NATASHA <klimknatasha@yandex.ru>
2024-11-06 22:48:39 +0300

    2

--- 2.c
+++ 2.c
@@ -4,3 +4,15 @@
 Данные на входе: Целое положительное число
 Данные на выходе: Одно слово: YES или NO
 */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int number;
+    scanf("%d", &number);
+
+    ((number >= 100) && (number <= 999)) ? printf("YES") : printf("NO");
+
+    return 0;
+}
