dea89c886e4eb8daf2c7d9d561f0b3233ffc6b61
NATASHA <klimknatasha@yandex.ru>
2024-11-06 23:35:36 +0300

    4

--- 4.c
+++ 4.c
@@ -5,3 +5,23 @@
 Данные на входе: Целое неотрицательное число
 Данные на выходе: Целое неотрицательное число наоборот
 */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int number, reverse = 0, order = 1;
+
+    scanf("%d", &number);
+
+    while ((number / order) > 0)
+    {
+        reverse *= 10;
+        reverse += ((number / order) % 10);
+        order *= 10;
+    }
+    
+    printf("%d", reverse);
+
+    return 0;
+}
